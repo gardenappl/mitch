035e15b44bbe5a708ca323206c298f772c12dcc4
gardenapple <mailbox@appl.garden>
2025-07-29 05:10:52 +0300

    Cleanup JS code for converting blob: to data:, confirmed to work
    Ren'Py deletes the blob: URL after 1 second, which is why it did not
    seem to work before, as I was too slow with my debugger :D
    
    Still need to handle big data: URLs.
    
    Fixes: https://todo.sr.ht/~gardenapple/mitch/66
    References: https://todo.sr.ht/~gardenapple/mitch/71

--- app/src/main/kotlin/ui/MitchWebView.kt
+++ app/src/main/kotlin/ui/MitchWebView.kt
@@ -23,31 +23,21 @@ class MitchWebView @JvmOverloads constructor(
 
     fun redirectBlobUrlToDataUrl(blobUrl: String, fileName: String) {
         evaluateJavascript("""
-            {
-                console.log("url")
-                console.log("$blobUrl")
-                fetch("$blobUrl").then(r => r.blob(), e => { console.error(e); console.error(e.stack);  }).then(blob => {
-                    console.log("loaded");
-                    console.log("blob: " + blob);
-                        // https://stackoverflow.com/a/30407959/5701177
-                        var reader = new FileReader();
-                        reader.onload = function(e) {
-                            console.log("oooooohh");
-                            // https://stackoverflow.com/a/15832662/5701177
-                            var link = document.createElement("a");
-                            link.download = "$fileName";
-                            link.href = e.target.result;
-                            document.body.appendChild(link);
-                            link.click();
-                            document.body.removeChild(link);
-                            delete link;
-                        }
-                        console.log("pre-read");
-                        reader.readAsDataURL(blob);
-                        console.log("post-read");
-                    }, e => console.error(e)
-                );
-            }
+            fetch("$blobUrl").then(r => r.blob()).then(blob => {
+                // https://stackoverflow.com/a/30407959/5701177
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    // https://stackoverflow.com/a/15832662/5701177
+                    var link = document.createElement("a");
+                    link.download = "$fileName";
+                    link.href = e.target.result;
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+                    delete link;
+                }
+                reader.readAsDataURL(blob);
+            });
             """, null)
     }
 }
